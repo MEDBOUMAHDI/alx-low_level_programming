a94901310aa1d45bd1eb3ef8ca22d09f0e8fb909
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-07 15:09:52 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+
 /**
-*main entry - point
-*
-*description: A C program that prints with puts function
-*
-*Return always 0 
+ * main entry - point
+ *
+ * description: A C program that prints with puts function
+ *
+ * Return always 0 (success) 
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
