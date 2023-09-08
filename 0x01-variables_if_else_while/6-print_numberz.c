bed581f6dd204984427809e2bf38d3ba7cf63967
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-08 13:05:18 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+
+	for (int a = 0; a <= 9; a++)
+	{
+		printf("%d ",a);
+	}
+	printf('\n');
+	return (0);
+}
