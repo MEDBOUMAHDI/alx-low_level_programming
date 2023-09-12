ea67888dd6d2bbe9a7d790d3bd766bfc8a43cdb8
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-12 04:30:51 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character m
+ * @m: The character to print
+ * Return: always success 1.
+ * On error, -1 is returned.
+ */
+int _putchar(char m)
+{
+	return (write(1, &m, 1));
+}
