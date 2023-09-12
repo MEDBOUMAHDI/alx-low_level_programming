99e517a307da43e4df6981c5b6fb6b026ac96579
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-12 11:19:51 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	int string[] = {85, 102, 127, 136, 88, 109, 67, 134, 177};
+	int string[] = {85, 102, 127, 136, 88, 109, 67, 134};
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(string[i]);
 	}
