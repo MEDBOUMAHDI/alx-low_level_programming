e2323e4d0f314c12c0f3fe27253c377f1e8715df
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-13 11:27:11 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * main - Prog that finds and prints the first 98 Fibonacci numbers.
+ * starting, with 1 and 2.
+ * followed by a new line.
+ * The numbers should be separated by comma.
+ * followed by a space .
+ * You are allowed to use the standard library.
+ * You are not allowed to use any other library (You can’t use GMP etc…).
+ * You are not allowed to use long long, malloc, pointers, arrays/tables, or structures.
+ * You are not allowed to hard code any Fibonacci number (except for 1 and 2).
+ * Return: ALways 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long int m, n, l, n1, n2, l1, l2;
+
+	n = 1;
+	l = 2;
+
+	printf("%lu", n);
+
+	for (m = 1; m < 91; m++)
+	{
+		printf(", %lu", l);
+		l = l + n;
+		n = l - n;
+	}
+
+	n1 = n / 1000000000;
+	n2 = n % 1000000000;
+	l1 = l / 1000000000;
+	l2 = l % 1000000000;
+
+	for (m = 92; m < 99; ++m)
+	{
+
+		printf(", %lu", l1 + (l2 / 1000000000));
+		printf("%lu", l2 % 1000000000);
+		l1 = l1 + n1;
+		n1 = l1 - n1;
+		l2 = l2 + n2;
+		n2 = l2 - n2;
+	}
+
+	printf("\n");
+
+
+	return (0);
+}
