458101017e96b2c00a6e86772a3e70bbcdb088bf
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-14 07:30:34 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
-
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -18,6 +9,4 @@ void print_number(int n);
 int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
-int mul(int, int);
-
-#endif /* MAIN_H */ 
+int mul(int, int); 
