ad3031600111abf44e3df64df57a607760cf13e9
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-14 09:34:13 -0700

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,15 +7,15 @@
 *Return: returns nothing
 */
 
-void print_square(int size)
+void print_square(int siz)
 {
 	int inc1, inc2;
 
-	if (size > 0)
+	if (siz > 0)
 	{
-		for (inc1 = 0; inc1 < size; inc1++)
+		for (inc1 = 0; inc1 < siz; inc1++)
 		{
-			for (inc2 = 0; inc2 < (size - 1); inc2++)
+			for (inc2 = 0; inc2 < (siz - 1); inc2++)
 			{
 				putchar('#');
 			}
