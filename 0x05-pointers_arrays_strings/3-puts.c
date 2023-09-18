2f5a69189cff3f1ac6a69abb0fe23379f54697d2
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-18 11:03:51 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,17 @@
-#include "holberton.h"
+#include"main.h"
 
 /**
- * _puts - Write a function that prints a string
- * @string: an input string
+ * _puts - prints a string, followed by a newline
+ * @string: string parameter to print
  * Return: Nothing
- */
+*/
+
 void _puts(char *string)
 {
-	int i = 0;
-
-	while (string[i] != '\0')
+	while (*string != '\0')
 	{
-		_putchar(string[i]);
-		i++;
+		_putchar(*string + 0);
+		++string;
 	}
 	_putchar('\n');
 }
