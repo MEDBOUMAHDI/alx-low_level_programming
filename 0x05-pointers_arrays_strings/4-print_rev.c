0d3bc9454508dfffd8ec33a59fccb3d26eb38272
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-18 11:16:53 -0700

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,10 @@ void print_rev(char *string)
 {
 	int i;
 
-	/*finds the length of string without null character*/
-	for (i = 0; string[i] != '\0'; i++);
-
-	/*print char from the last index as you decrement*/
-	for (--i; i >= 0; i--)
+	for (i = 0; string[i] != '\0'; i++){
+	for (--i; i >= 0; i--){
 		_putchar(string[i]);
+	}
 	_putchar('\n');
+	}
 }
