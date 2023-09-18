d86f64531d5c403aa75307141ce472725d7fa47a
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-18 10:12:13 -0700

    Don't swap horses in crossing a stream

---
+++ 1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - swaps the values of two integers
+ * @a: input parameter 1
+ * @b: input parameter 2
+ * Return: Nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
