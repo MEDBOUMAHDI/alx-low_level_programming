66ee2ff14e0fc7e82f3e353f6ad8a2666d886c78
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-18 12:46:12 -0700

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,23 @@
 #include "main.h"
 #include <string.h>
 /**
- * puts_half -   prints half of a string, followed by a new line.
- * @str: char pointer
- * Return: the length *s
+ * puts_half -   prints half of a string
+ * followed by a new line.
+ * @string: char pointer
+ * Return: Nothing
  */
-void puts_half(char *str)
+void puts_half(char *string)
 {
 	int len, i;
 
-	len = strlen(str);
+	len = strlen(string);
 	if (len % 2 != 0)
 		i = (len / 2) + 1;
 	else
 		i = len / 2;
 	for (; i < len; i++)
 	{
-		_putchar(str[i]);
+		_putchar(string[i]);
 	}
 	_putchar('\n');
 }
