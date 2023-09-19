33292a004be1fb149136f646d25e408fb8a623c7
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-19 01:24:48 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,7 @@
  */
 int _atoi(char *str)
 {
-	unsigned int x;
+	unsigned int x = 0;
 	unsigned int i;
 	char negative;
 	char positive;
@@ -14,7 +14,7 @@ int _atoi(char *str)
 	positive = 0;
 	negative = 0;
 	i = 0;
-	x = 0;
+	//x = 0;
 
 	while (str[i] != '\0')
 	{
