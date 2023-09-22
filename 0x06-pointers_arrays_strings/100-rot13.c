12ad002cfc9c31e30f65fde3221f75ffd2ed54df
BOUMAHDI MOHAMED <med.boumahdi.med@gmail.com>
2023-09-22 01:17:18 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string into  rot13.
+ * @source: string refrance
+ * Return: *source encodd string into  rot13.
+ */
+char *rot13(char *source)
+{
+	int i = 0;
+
+	while (source[i] != '\0')
+	{
+		while (((source[i] >= 'a') && (sourc[i] <= 'z'))
+		|| ((source[i] >= 'A') && (source[i] <= 'Z')))
+		{
+			if (((source[i] >= 'a') && (source[i] <= 'm'))
+			|| ((source[i] >= 'A') && (source[i] <= 'M')))
+			{
+				source[i] = source[i] + 13;
+				break;
+			}
+			source[i] = source[i] - 13;
+			break;
+		}
+		i++;
+	}
+	return (source);
+}
