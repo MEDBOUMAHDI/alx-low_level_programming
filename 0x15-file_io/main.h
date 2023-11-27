653528d42d4797d919911ad2fe1d14fa3c60b1e5
medboumahdi <med.boumahdi.med@gmail.com>
2023-11-27 02:14:05 -0800

    Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,6 @@
+#ifndef main_H
+#define main_H
+
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -5,3 +8,5 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 int _putchar(char c);
+
+#endif
