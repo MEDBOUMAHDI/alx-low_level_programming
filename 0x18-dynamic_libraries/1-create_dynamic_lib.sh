b1e21816efa629b8bab2d38300891022a233bfa2
medboumahdi <med.boumahdi.med.com>
2024-01-15 13:12:25 -0800

    Without libraries what have we? We have no past and no future

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# File: 1-create_dynamic_lib.sh
-# Author: Alex Orland Arévalo Tribaldos
-# email: <3915@holbertonschool.com>
-gcc -fPIC -c *.c
-gcc -shared -o liball.so *.o
+        gcc -wall -wextre -werror -pedantic -c -fPIC *.c
+        gcc -shared -o liball.so *.o
+        export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
